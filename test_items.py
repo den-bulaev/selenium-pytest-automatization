e53061f919cde3a6e228cdaffd5bc963bf9bbfca
python-dude <all.nickname.is.busy@gmail.com>
2019-12-29 12:04:51 +0200

    test

---
+++ test_items.py
@@ -0,0 +1,11 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_add_to_basket_button(browser):
+    browser.get(link)
+    btn = browser.find_elements_by_css_selector("button.btn-add-to-basket")
+    assert len(btn) == 1, "{}".format("button not found" if len(btn) < 1 else "selector is not unique")
+#   меньше 1 элемента в списке - кнопки нет; в ином случае - селектор не уникальный
+#   если в списке 1 элемент - AssertionError не вызывается
+
+# для проверки добавьте time.sleep(30) сразу после открытия ссылки
